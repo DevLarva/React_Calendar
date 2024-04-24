c1f7d271656fec235c7c0e3896ba5ade2eb97795
백대홍 <bdh3620@naver.com>
2024-04-24 16:45:58 +0900

    Feat: Small Calendar pointer Add

--- src/Components/SmallCalendar.js
+++ src/Components/SmallCalendar.js
@@ -8,12 +8,31 @@ export default function SmallCalendar() {
     useEffect(() => {
         setCurrentMonth(getMonth(currentMonthIdx))
     }, [currentMonthIdx])
+    
+    function handlePrevMonth() {
+        setCurrentMonthIdx(currentMonthIdx -1)
+    }
+    function handleNextMonth() {
+        setCurrentMonthIdx(currentMonthIdx +1)
+    }
+
   return (
     <div className="mt-9">
         <header className="flex justify-between">
             <p className="text-gray-500 font-bold">
                 {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
             </p>
+            <button onClick={handlePrevMonth}>
+                <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+                    chevron_left
+                </span>
+            </button>
+
+            <button onClick={handleNextMonth}>
+                <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+                    chevron_right
+                </span>
+            </button>
         </header>
       
     </div>
