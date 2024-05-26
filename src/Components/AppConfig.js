7d395514f9052e7ad9cf224bdf850d0a1f135893
백대홍 <bdh3620@naver.com>
2024-05-26 21:18:25 +0900

    AppConfig.js 수정(if문 수정)

--- src/Components/AppConfig.js
+++ src/Components/AppConfig.js
@@ -4,6 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://andnproject-env.eba-vrmatduy.ap-northeast-2.elasticbeanstalk.com";
+} else {
+  backendHost = "http://andnproject-env.eba-vrmatduy.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
