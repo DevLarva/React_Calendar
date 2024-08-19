ab9ab69c8d8be5d6caa68a11ca36013a441ad8a1
백대홍 <bdh3620@naver.com>
2024-08-19 22:05:20 +0900

    Feat: 달력에서 일정 이어지게 보이게끔 수정

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -20,6 +20,23 @@ export default function Day({ day, rowIdx }) {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? 'bg-blue-600 text-white rounded-full w-7' : '';
   }
 
+  function getEventPositionClass(evt) {
+    const isStartDay = dayjs(day).isSame(dayjs(evt.startDate), 'day');
+    const isEndDay = dayjs(day).isSame(dayjs(evt.endDate), 'day');
+    const isSingleDay = isStartDay && isEndDay;
+
+    if (isSingleDay) {
+      return 'rounded-lg';  // 모든 모서리가 약간 둥근 사각형
+    } else if (isStartDay) {
+      return 'rounded-l-lg'; // 왼쪽 모서리가 둥근 사각형
+    } else if (isEndDay) {
+      return 'rounded-r-lg'; // 오른쪽 모서리가 둥근 사각형
+    } else {
+      return 'rounded-none'; // 모서리 둥근 모양 없음 (직각 사각형)
+    }
+  }
+
+
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -41,10 +58,17 @@ export default function Day({ day, rowIdx }) {
           <div
             key={idx}
             onClick={() => setSelectedEvent(evt)}
-            className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+            className={`bg-${evt.label}-200 p-1.5 text-gray-600 text-sm mb-2 truncate ${getEventPositionClass(evt)}`}
+            style={{
+              marginRight: dayjs(day).isSame(dayjs(evt.endDate), 'day') ? '2px' : '0',
+              paddingLeft: '2px',
+              paddingRight: '2px',
+              width: 'calc(100% + 2px)',  // 이 부분은 스타일이 균일하게 채워지도록 합니다.
+            }}
           >
             {evt.title}
           </div>
+
         ))}
       </div>
     </div>
